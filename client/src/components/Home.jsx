09a303fcd52f2b4470361a84d658fe5bda55c224
Jay Jagadish Behera <jayjagadishb@gmail.com>
2024-09-17 19:09:56 +0530

    adding client code

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import laptopImg from "../assets/laptop.jpg";
+import phoneImg from "../assets/mobile.jpg";
+import tabletImg from "../assets/tablet.jpg";
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <div className="flex flex-col space-y-4 md:space-y-6 lg:space-y-8">
+        <div className="bg-gray-200  p-4 rounded md:p-6 lg:p-8 h-80 flex flex-col justify-between">
+          <img
+            src={laptopImg}
+            alt="Laptop"
+            className="w-full h-48 object-contain"
+          />
+          <h2 className="text-xl font-bold mt-2 md:text-2xl lg:text-3xl">
+            Laptop
+          </h2>
+        </div>
+
+        <div className="bg-gray-200 p-4 rounded md:p-6 lg:p-8 h-80 flex flex-col justify-between">
+          <img
+            src={phoneImg}
+            alt="Phone"
+            className="w-full h-48 object-contain"
+          />
+          <h2 className="text-xl font-bold mt-2 md:text-2xl lg:text-3xl">
+            Phone
+          </h2>
+        </div>
+        <div className="bg-gray-200 p-4 rounded md:p-6 lg:p-8 h-80 flex flex-col justify-between">
+          <img
+            src={tabletImg}
+            alt="Tablet"
+            className="w-full h-48 object-contain"
+          />
+          <h2 className="text-xl font-bold mt-2 md:text-2xl lg:text-3xl">
+            Tablet
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
